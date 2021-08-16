78f7fb605771139063c0a9ddb1c8a5d11fac8c77
James Tucker <jamesctucker@protonmail.com>
2021-08-15 23:24:37 -0500

    Add card for updating title and note

--- components/todos/Todo.jsx
+++ components/todos/Todo.jsx
@@ -1,30 +1,122 @@
-import { ListItem, Flex, Checkbox, Text } from "@chakra-ui/react";
+import { useState, useRef, useEffect } from "react";
+import {
+  ListItem,
+  Flex,
+  Checkbox,
+  Text,
+  Input,
+  Textarea,
+  useOutsideClick,
+  Button,
+  ButtonGroup,
+} from "@chakra-ui/react";
 import useCompleteTodo from "../../hooks/useCompleteTodo";
+import useUpdateTodo from "../../hooks/useUpdateTodo";
 
 export function Todo({ todo }) {
+  const ref = useRef();
+  const [title, setTitle] = useState(todo.title);
+  const [note, setNote] = useState(todo.note);
+  const [showMoreOptions, setShowMoreOptions] = useState(false);
+  const [updated, setUpdated] = useState(false);
+
   const completeTodoMutation = useCompleteTodo({ todo });
-  const handleCheck = (e) => {
+  function handleCheck(e) {
     if (e.target.checked) {
       setTimeout(() => {
         completeTodoMutation.mutate();
       }, 400);
     }
-  };
+  }
+
+  useOutsideClick({
+    ref: ref,
+    handler: () => {
+      setShowMoreOptions(false);
+      setTitle(todo.title);
+      setNote(todo.note);
+    },
+  });
+
+  function checkMatching() {
+    if (title !== todo.title || note !== todo.note) {
+      setUpdated(true);
+      return;
+    }
+    setUpdated(false);
+  }
+
+  useEffect(() => {
+    checkMatching();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [title, note]);
+
+  const updateTodoMutation = useUpdateTodo({
+    todo: { id: todo.id, title: title, note: note },
+  });
+
+  async function saveUpdate() {
+    await updateTodoMutation.mutate();
+    setShowMoreOptions(false);
+  }
 
   return (
     <>
-      <ListItem>
-        <Flex direction="row" m={2}>
-          <Checkbox
+      {!showMoreOptions && (
+        <ListItem onDoubleClick={() => setShowMoreOptions(true)}>
+          <Flex direction="row" m={2}>
+            <Checkbox
+              onChange={(e) => {
+                handleCheck(e);
+              }}
+              mr={2}
+            />
+            <Text fontSize="md">{title}</Text>
+            {/* {errorMsg && <p>{errorMsg}</p>} */}
+          </Flex>
+        </ListItem>
+      )}
+      {showMoreOptions && (
+        <Flex
+          ref={ref}
+          boxShadow="md"
+          p="4"
+          my="4"
+          rounded="md"
+          border="1px"
+          borderColor="gray.600"
+          flexDirection="column"
+        >
+          <Input
+            placeholder={todo.title}
+            variant="unstyled"
             onChange={(e) => {
-              handleCheck(e);
+              setTitle(e.target.value);
             }}
-            mr={2}
+            value={title}
           />
-          <Text fontSize="md">{todo.task}</Text>
-          {/* {errorMsg && <p>{errorMsg}</p>} */}
+          <Textarea
+            placeholder="Notes"
+            variant="unstyled"
+            onChange={(e) => {
+              setNote(e.target.value);
+            }}
+            value={note}
+          />
+          {updated && (
+            <ButtonGroup variant="outline" spacing="6">
+              <Button
+                onClick={() => saveUpdate()}
+                colorScheme="blue"
+                isLoading={updateTodoMutation.isLoading}
+              >
+                Save
+              </Button>
+              <Button>Cancel</Button>
+            </ButtonGroup>
+          )}
         </Flex>
-      </ListItem>
+      )}
     </>
   );
 }
